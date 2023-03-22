e8ef337a10805aea6f126c90103ea3b138c81e3a
sovmulax <soumailaevane@gmail.com>
2023-03-22 14:54:20 +0000

    zik

--- functions/components/achat_billet.c
+++ functions/components/achat_billet.c
@@ -4,8 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-int achat_billet(char a[200], char b[200], char c[200], char d[200])
-{
- 
-    return (0);
+int achat_billet(sqlite3* db, int id, int cine) {
+    int nbOccurences = 0;
+    int nbPlaces = 0;
+    char* errorMsg = NULL;
+    sqlite3_stmt* stmt;
+
+    // Compte le nombre d'occurrences dans la table "billets" pour l'événement spécifié
+    sqlite3_prepare_v2(db, "SELECT COUNT(*) FROM billets WHERE idevent = ?", -1, &stmt, NULL);
+    sqlite3_bind_int(stmt, 1, id);
+    sqlite3_step(stmt);
+    nbOccurences = sqlite3_column_int(stmt, 0);
+    sqlite3_finalize(stmt);
+
+    // Récupère le nombre de places disponibles pour l'événement spécifié
+    sqlite3_prepare_v2(db, "SELECT nbplace FROM events WHERE id = ?", -1, &stmt, NULL);
+    sqlite3_bind_int(stmt, 1, id);
+    sqlite3_step(stmt);
+    nbPlaces = sqlite3_column_int(stmt, 0);
+    sqlite3_finalize(stmt);
+
+    // Si le nombre d'occurrences est inférieur au nombre de places disponibles, insère une nouvelle occurrence dans la table "billets"
+    if (nbOccurences < nbPlaces) {
+        sqlite3_prepare_v2(db, "INSERT INTO billets (idevent, idseance) VALUES (?, NULL)", -1, &stmt, NULL);
+        sqlite3_bind_int(stmt, 1, id);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+        return 1;
+    }
+
+    return 0;
 }
