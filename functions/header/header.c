e433f7847cd9adf26eff406968691b512ff6424b
sovmulax <soumailaevane@gmail.com>
2023-03-26 20:09:16 +0000

    header update

--- functions/header/header.c
+++ functions/header/header.c
@@ -1,3 +1,4 @@
 void add_event(sqlite3 *db, char a[200], char b[200], char c[200], char d[200], char e[200]);
 void event_list(sqlite3 *db, const char *columns);
 int achat_billet(sqlite3 *db, int id, int cine);
+void ajout_seance(sqlite3 *db, char *element1, char *element2);
